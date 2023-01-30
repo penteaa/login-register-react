8c5317065b557cee8af8eccaa52e40589a90211e
Penteaa <p_beny_04@yahoo.com>
2023-01-30 17:52:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import './App.css';
+import { Register } from "./Register";
+import { Login } from "./Login";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ?   <Login onFormSwitch={toggleForm} /> : <Register  onFormSwitch={toggleForm}/>
+      }
+   
+    </div>
+  );
+}
+
+export default App;
